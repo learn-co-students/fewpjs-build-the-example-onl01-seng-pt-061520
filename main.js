adc219798a3baa4060610e729edb9e3f18b9f64b
ajkelso <ajkelso@gmail.com>
2021-01-04 13:32:00 -0500

    Done.

--- main.js
+++ main.js
@@ -3,7 +3,29 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const hearts = document.querySelectorAll(".like-glyph")
+const modal = document.getElementById("modal")
+const modalMessage = document.getElementById("modal-message")
 
+hearts.forEach(heart => heart.addEventListener('click', function(e){
+  mimicServerCall()
+  .then(res => res.json)
+  .then(function(){
+    if (heart.innerText == EMPTY_HEART) {
+      heart.innerText = FULL_HEART
+      heart.classList.add('activated-heart')
+    } else {
+      heart.innerText = EMPTY_HEART
+      heart.classList.remove('activated-heart')
+    }
+  })
+  .catch(function(error) {
+    modal.innerHTML = `<h2>${error}</h2>`
+    modal.classList.remove("hidden")
+    setTimeout(function(){ modal.classList.add("hidden")}, 5000)
+  })
+  
+}))
 
 
 
