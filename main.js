7d883ffad297b6d24aaa62cde5160be16a2e89e9
github username <github email address>
2021-01-11 15:28:23 -0800

    Done.

--- main.js
+++ main.js
@@ -3,7 +3,41 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+function toggleLiked(likeButton) {
+  if (likeButton.className === 'like') {
+    likeButton.className = 'like activated-heart';
+    likeButton.children[0].innerText = FULL_HEART;
+  } else {
+    likeButton.className = 'like';
+    likeButton.children[0].innerText = EMPTY_HEART;
+  }
+}
 
+function displayErrorMessage(message) {
+  document.getElementById('modal-message').innerText = message;
+  document.getElementById('modal').className = '';
+  setTimeout(function() {
+    document.getElementById('modal').className = 'hidden';
+  }, 5000);
+}
+
+function addListenersToLikeButtons(likeButtons) {
+  for (const likeButton of likeButtons) {
+    likeButton.addEventListener('click', function(e) {
+      e.preventDefault();
+      mimicServerCall().then(function(response) {
+        toggleLiked(likeButton);
+      }).catch(function(error) {
+        displayErrorMessage(error);
+      });
+    });
+  }
+}
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  addListenersToLikeButtons(document.getElementsByClassName('like'))
+});
 
 
 
@@ -14,7 +48,7 @@ const FULL_HEART = '♥'
 function mimicServerCall(url="http://mimicServer.example.com", config={}) {
   return new Promise(function(resolve, reject) {
     setTimeout(function() {
-      let isRandomFailure = Math.random() < .2
+      const isRandomFailure = Math.random() < .2
       if (isRandomFailure) {
         reject("Random server error. Try again.");
       } else {
